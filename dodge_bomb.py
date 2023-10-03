31ec8d2439b9a6ae66524305e61b173c367707d8
unknown <c0b2207762@edu.teu.ac.jp>
2023-10-03 14:28:25 +0900

    練習2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,22 +1,23 @@
 import random
 import sys
 import pygame as pg
-
-
 WIDTH, HEIGHT = 1600, 900
-
-
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    bd_img = pg.Surface((20, 20))  # 練習１：爆弾Surfaceを作成する
+    bd_img.set_colorkey((0, 0, 0))  # 練習１：黒い部分を透明にする
+    pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
+    bd_rct = bd_img.get_rect()  # 練習１：SurfaceからRectを抽出する
+    x, y = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    bd_rct.center = (x, y)  # 練習１：Rectにランダムな座標を設定する
+    vx, vy = +5, +5  # 練習２：爆弾の速度
+
     clock = pg.time.Clock()
     tmr = 0
-    bomb = pg.Surface((20,20))
-    pg.draw.circle(bomb, (255, 0, 0), (10, 10), 10)
-    bomb.set_colorkey((0, 0, 0))
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -24,13 +25,12 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-        draw_bomb = bomb.get_rect()
-        x, y = random.randint(1, WIDTH), random.randint(1, HEIGHT)
-        draw_bomb.center = (x, y)  # Rectにランダムな座標を与える
-        screen.blit(bomb, draw_bomb)  # ここまで練習1
+        bd_rct.move_ip(vx, vy)  # 練習２：爆弾を移動させる
+        screen.blit(bd_img, bd_rct)  # 練習１：Rectを使って試しにblit
         pg.display.update()
         tmr += 1
         clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
