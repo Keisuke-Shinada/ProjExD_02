5b431d256c981fe77326cfd5298fb95a386904f8
unknown <c0b2207762@edu.teu.ac.jp>
2023-10-03 14:46:16 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -8,28 +8,42 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = (900, 400)
+    
     bd_img = pg.Surface((20, 20))  # 練習１：爆弾Surfaceを作成する
     bd_img.set_colorkey((0, 0, 0))  # 練習１：黒い部分を透明にする
     pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
     bd_rct = bd_img.get_rect()  # 練習１：SurfaceからRectを抽出する
     x, y = random.randint(0, WIDTH), random.randint(0, HEIGHT)
     bd_rct.center = (x, y)  # 練習１：Rectにランダムな座標を設定する
+    
     vx, vy = +5, +5  # 練習２：爆弾の速度
+    delta = {pg.K_UP:(0, -5), pg.K_DOWN:(0, +5), pg.K_LEFT:(-5, 0), pg.K_RIGHT:(+5, 0)}
 
     clock = pg.time.Clock()
     tmr = 0
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        sum_mv = [0, 0]
+        key_lst = pg.key.get_pressed()
+        for key , mv in delta.items():
+            if key_lst[key]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+        kk_rct.move_ip(sum_mv[0], sum_mv[1])
+        screen.blit(kk_img, kk_rct)  # 練習３：こうかとん動かす
+        
         bd_rct.move_ip(vx, vy)  # 練習２：爆弾を移動させる
         screen.blit(bd_img, bd_rct)  # 練習１：Rectを使って試しにblit
         pg.display.update()
         tmr += 1
-        clock.tick(10)
         clock.tick(50)
 
 
